c1e566f9e3db9b30f68279fb0b282949bead14ed
slaklin <120272257+slaklin@users.noreply.github.com>
2023-08-24 00:37:51 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# counting_the_salary
- 
+# Programming vacancies compare
+This project uses the web services api hh.ru and superjob.ru to search for programmer vacancies in various programming languages and identify the average salary for each of them separately. Outputs the result for services in the form of a table.
+
+## How to install
+1. Create [api.superjob](https://api.superjob.ru/2.0/) Generate API Key for work with API.
+
+#### Requirements
+
+Python3 should be already installed. Then use pip to install dependencies:
+```
+pip install -r requirements.txt
+```
+### Create an environment
+
+#### Environment variables
+
+- SECRET_KEY
+
+1. Place the `.env` file in the root folder of your project.
+2. `.env` contains text data without quotes.
+
+For example, if you print `.env` content, you will see:
+
+```
+$ cat .env
+SECRET_KEY=1a5d754733b015ghty67660143c70238efa4esad1taec48
+```
+## Example of running a script
+```
+C:\Users\Training\counting_the_salary\counting_the_salary\counting_the_salary.py
+```
+## Project Goals
+This code was written for educational purposes as part of an online course for web developers at [dvmn.org.](https://dvmn.org/)
+
---
+++ counting_the_salary.py
@@ -0,0 +1,140 @@
+import requests
+import os
+
+from dotenv import load_dotenv
+from terminaltables import AsciiTable
+
+
+def calculating_the_number_vacancies_hh(language):
+    number_of_vacancies = []
+    list_of_vacancies = []
+    page = 0
+    pages_number = 18
+    while page < pages_number:
+        url = 'https://api.hh.ru/vacancies/'
+        payload = {
+            "text": f'{language}',
+            "area": 1,
+            "page": f'{page}',
+        }
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        print(response.status_code)
+        job_description = response.json()
+        page += 1
+        list_of_vacancies.append(job_description)
+        job_openings = job_description['found']
+        number_of_vacancies.append(job_openings)
+    print(number_of_vacancies)
+    return number_of_vacancies, list_of_vacancies
+
+
+def predict_rub_salary_for_hh(list_of_vacancies):
+    average_salary = []
+    for number in range(len(list_of_vacancies)):
+        for vacancy in list_of_vacancies[number]['items']:
+            if vacancy['salary'] is None or vacancy['salary']['currency'] != 'RUR':
+                average_salary.append(None)
+            else:
+                if vacancy['salary']['from'] and vacancy['salary']['to']:
+                    salary = (vacancy['salary']['from'] + vacancy['salary']['to']) / 2
+                elif vacancy['salary']['from']:
+                    salary = vacancy['salary']['from'] * 1.2
+                elif vacancy['salary']['to']:
+                    salary = vacancy['salary']['to'] * 0.8
+                else:
+                    salary = None
+                average_salary.append(salary)
+    return average_salary
+
+
+def calculating_the_number_vacancies_sj(language):
+    load_dotenv()
+    super_job_secret_key = os.getenv("SECRET_KEY")
+    number_of_vacancies = []
+    list_of_vacancies = []
+    page = 0
+    pages_number = 18
+    while page < pages_number:
+        url = 'https://api.superjob.ru/2.0/vacancies/'
+        headers = {
+            "X-Api-App-Id": super_job_secret_key,
+        }
+        params = {
+            'town': "Москва",
+            'keyword': f'{language}',
+            "page": f'{page}',
+        }
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+        job_description = response.json()
+        page += 1
+        list_of_vacancies.append(job_description)
+        job_openings = job_description['total']
+        number_of_vacancies.append(job_openings)
+    return number_of_vacancies, list_of_vacancies
+
+
+def predict_rub_salary_for_sj(list_of_vacancies):
+    average_salary = []
+    for number in range(len(list_of_vacancies)):
+        for vacancy in list_of_vacancies[number]['objects']:
+            if vacancy['payment_from'] and vacancy['payment_to']:
+                salary = (vacancy['payment_from'] + vacancy['payment_to']) / 2
+            elif vacancy['payment_from']:
+                salary = vacancy['payment_from'] * 1.2
+            elif vacancy['payment_to']:
+                salary = vacancy['payment_to'] * 0.8
+            else:
+                salary = None
+            average_salary.append(salary)
+    return average_salary
+
+
+def computation_work_for_vacancies(average_salary):
+    vacancies_processed = 0
+    specified_salary = []
+    list_jobs_with_salary = []
+    statistical_average_salary = []
+    for amount_of_money in average_salary:
+        if amount_of_money is not None:
+            specified_salary.append(amount_of_money)
+            vacancies_processed += amount_of_money
+    jobs_with_salary = (len(specified_salary))
+    list_jobs_with_salary.append(jobs_with_salary)
+    average_salary_the_specialty = vacancies_processed / jobs_with_salary
+    statistical_average_salary.append(int(average_salary_the_specialty))
+    return statistical_average_salary, list_jobs_with_salary
+
+
+def main():
+    vacancies = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'CSS', 'C#', 'C']
+    # vacancies = ['C++', 'CSS', 'C#', 'C']
+    # salary_table_sj = [
+    # ['SuperJob Moscow'],
+    # ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
+    # ]
+    # for language in vacancies:
+    #     number_of_vacancies, list_of_vacancies = calculating_the_number_vacancies_sj(language)
+    #     average_salary = predict_rub_salary_for_sj(list_of_vacancies)
+    #     statistical_average_salary, list_jobs_with_salary = computation_work_for_vacancies(average_salary)
+    #     salary_table_sj.append([language, number_of_vacancies[0], list_jobs_with_salary[0], statistical_average_salary[0]])
+    # table_superjob = AsciiTable(salary_table_sj)
+    # print(table_superjob.table)
+
+    salary_table_hh = [
+    ['HeadHunter Moscow'],
+    ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
+    ]
+    for language in vacancies:
+        number_of_vacancies, list_of_vacancies = calculating_the_number_vacancies_hh(language)
+        average_salary = predict_rub_salary_for_hh(list_of_vacancies)
+        statistical_average_salary, list_jobs_with_salary = computation_work_for_vacancies(average_salary)
+        salary_table_hh.append(
+            [language, number_of_vacancies[0], list_jobs_with_salary[0], statistical_average_salary[0]])
+    table_headhunter = AsciiTable(salary_table_hh)
+    print(table_headhunter.table)
+
+
+if __name__ == "__main__":
+    main()
